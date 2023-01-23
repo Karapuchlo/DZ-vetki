d6e6aca94c4cf1c8578fd580c960766dd2dced45
aleksey <mc_gra_dy@mail.ru>
2023-01-24 00:23:47 +0300

    добавлена функция заглавной первой буквы

--- str_func.py
+++ str_func.py
@@ -1,4 +1,11 @@
+#функция заглавных
 def Up(s1):
     s1 = 'abcdef'
     s2 = s1.upper()
     return s2;
+
+#функция заглавной первой буквы
+def UpTitle(x1):
+    x1 = 'привеи андрей'
+    x2 = x1.title()
+    return x2;
